ca488a20b95766b41b13eba3504d10fd49dbe234
marioesteban11 <marioesteban00@gmail.com>
2022-05-13 10:13:57 +0200

    stack sin assert

--- Practica_9/stack.c
+++ Practica_9/stack.c
@@ -16,12 +16,21 @@ stack* create_stack(int max_size, int elemSize)
 	
 	stack *s = malloc(sizeof(stack));
 	pthread_mutex_lock(&s->mutex);
-	assert(elemSize > 0);
+	if (elemSize <=0){
+		fprintf(stderr, "Numero de elementos no valido\n");
+		return s;
+	}
+	//assert(elemSize > 0);
 	s->elemSize = elemSize;
 	s->logLength = 0;
 	s->allocLength = max_size;
 	s->elems = malloc(max_size * elemSize);
-	assert(s->elems != NULL);
+	if (s->elems == NULL)
+	{
+		fprintf(stderr, "Error al crear elems\n");
+		return s;
+	}
+	//assert(s->elems != NULL);
 	
 	pthread_mutex_unlock(&s->mutex);
 	return s;
@@ -50,13 +59,17 @@ void push(stack *s, const void *elemAddr)
 	if(s->logLength == s->allocLength){
 		s->allocLength *= 2;
 		s->elems = realloc(s->elems, s->allocLength * s->elemSize);
-		assert(s->elems != NULL);
+		if (s->elems == NULL)
+		{
+			fprintf(stderr, "Error al modificar elems\n");
+			return;
+		}
+		//assert(s->elems != NULL);
 	}
 	
 	destAddr = (char *)s->elems + s->logLength * s->elemSize;
 	memcpy(destAddr, elemAddr, s->elemSize);
 	s->logLength++;
-	//fprintf(stderr, "LOG LENGTH %d\n", s->logLength);
 	pthread_mutex_unlock(&s->mutex);
 }
 
@@ -64,7 +77,11 @@ void pop(stack *s, void *elemAddr)
 {
 	pthread_mutex_lock(&s->mutex);
 	const void *sourceAddr;
-	assert(!is_empty(s));
+	if (is_empty(s)){
+		fprintf(stderr, "No se puede hacer pop de una stack vacia\n");
+		return;
+	}
+	//assert(!is_empty(s));
 	s->logLength--;
 	sourceAddr = (const char *)s->elems + s->logLength * s->elemSize;
 	memcpy(elemAddr, sourceAddr, s->elemSize);
